1a61d172e8aed67b266d2c05a651b23cb7172bb8
valixxx1 <vgershun@gmail.com>
2024-04-18 09:09:55 +0300

    png_hdr

---
+++ errs.h
@@ -0,0 +1,44 @@
+/*  MIT License
+ *
+ *  Copyright (c) 2024 valixxx1
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#ifndef ERRS_H
+#define ERRS_H
+
+#include <stdlib.h>
+
+#define err(msg, code)  \
+  do {                  \
+    fputs(msg, stderr); \
+    exit(code);         \
+  } while (0)
+
+#define pbyte(byte) \
+  printf("%02X\n", (u8) byte)
+
+#define _2fewargs "Too few arguments!\n"
+#define fnread    "File wasn't read!\n"
+
+#else
+#warning errs.h double including!
+
+#endif
